a8a2a19e2960e38e91f08b77c165c382b88a2f51
Andrew <andrewdfabrizio@gmail.com>
2021-10-17 00:54:15 -0400

    Basic scrapy framework

---
+++ airbnb_scraper/airbnb_scraper/spiders/listings_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+from scrapy_splash import SplashRequest
+
+class ListingsSpider(scrapy.Spider):
+    name = "listings"
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+            'https://www.airbnb.com/rooms/36480178/'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f'quotes-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')

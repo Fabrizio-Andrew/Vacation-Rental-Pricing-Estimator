c2afc2d51b13d97a2bb7c76a065928a9f2a677dd
Andrew <andrewdfabrizio@gmail.com>
2021-11-03 00:19:31 -0400

    Settings and data structures for estimator

---
+++ linear-model.py
@@ -0,0 +1,16 @@
+coefs = {
+    'host_resp_over_few_days': -0.18180501,
+    'host_response_under_hours': 0.10483031,
+    'host_response_under_day': -0.15250934,
+    'dist_to_landmark': -0.32107893,
+    'longitude': -0.05541475,
+    'latitude': -0.03938244,
+    'room_type_hotel': 0.55789099,
+    'room_type_private': -0.36701204,
+    'review_scores_rating': 0.01080494,
+    'beds': 0.25154944,
+    'accommodates': 0.39175801
+}
+
+bias = 0.12633143197779734
+

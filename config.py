1c7cf5911ec2b9f181a0c80c77e218821533db5a
Andrew <andrewdfabrizio@gmail.com>
2021-11-10 23:55:25 -0500

    pushing to see if SSL is blocking authentication

---
+++ config.py
@@ -0,0 +1,31 @@
+import os
+import json
+
+# An environment variable for "ENVIRONMENT" must be set to "staging", or "production" for those
+# environments. If environment = "staging" or "production", env variables for AUTH0_CLIENT_ID 
+# & AUTH0C_CLIENT_SECRET are also required.
+
+if os.environ.get('ENVIRONMENT') == 'staging':
+    AUTH0_CLIENT_ID = os.environ.get('AUTH0_CLIENT_ID')
+    AUTH0_CLIENT_SECRET = os.environ.get('AUTH0_CLIENT_SECRET')
+    API_BASE_URL = 'https://immense-mountain-68865.herokuapp.com'
+
+elif os.environ.get('ENVIRONMENT') == 'production':
+    AUTH0_CLIENT_ID = os.environ.get('AUTH0_CLIENT_ID')
+    AUTH0_CLIENT_SECRET = os.environ.get('AUTH0_CLIENT_SECRET')
+    API_BASE_URL = 'https://vacation-rental-estimator-prod.herokuapp.com'
+
+else:
+    f = open('secrets.json')
+    secrets = json.load(f)
+    
+    AUTH0_CLIENT_ID = secrets['AUTH0_CLIENT_ID']
+    AUTH0_CLIENT_SECRET = secrets['AUTH0_CLIENT_SECRET']
+    API_BASE_URL = 'https://localhost:5000'
+
+AUTH0_ACCESS_TOKEN_URL = API_BASE_URL + '/oauth/token'
+AUTH0_AUTHORIZE_URL = API_BASE_URL + '/authorize'
+AUTH0_CALLBACK_URL = API_BASE_URL + '/callback'
+AUTH0_CLIENT_KWARGS = {
+    'scope': 'openid profile email'
+}
--- routes.py
+++ routes.py
@@ -1,14 +1,86 @@
-from flask import Flask, request, make_response, render_template
+import os
+from flask import Flask, request, make_response, render_template, session, redirect, url_for
 import json
+from authlib.integrations.flask_client import OAuth
+from functools import wraps
+from six.moves.urllib.parse import urlencode
+
 from standardization import standardize, destandardizePrice, standards
 from linearmodel import coefs, bias
 from percentile import calculatePercentile
+import config
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = config.AUTH0_CLIENT_SECRET
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_PERMANENT'] = False
+
+oauth = OAuth(app)
+
+auth0 = oauth.register(
+    'auth0',
+    client_id = config.AUTH0_CLIENT_ID,
+    client_secret = config.AUTH0_CLIENT_SECRET,
+    api_base_url=config.API_BASE_URL,
+    access_token_url=config.AUTH0_ACCESS_TOKEN_URL,
+    authorize_url=config.AUTH0_AUTHORIZE_URL,
+    client_kwargs=config.AUTH0_CLIENT_KWARGS
+)
+
+@app.route('/callback')
+def callback_handling():
+    # Handles response from token endpoint
+    auth0.authorize_access_token()
+    resp = auth0.get('userinfo')
+    userinfo = resp.json()
+
+    # Store the user information in flask session.
+    session['jwt_payload'] = userinfo
+    session['profile'] = {
+        'user_id': userinfo['sub'],
+        'name': userinfo['name'],
+        'picture': userinfo['picture']
+    }
+    return redirect('/')
+
+
+@app.route('/login')
+def login():
+    return auth0.authorize_redirect(redirect_uri=config.AUTH0_CALLBACK_URL)
+
+
+@app.route('/logout')
+def logout():
+    # Clear session stored data
+    session.clear()
+    # Redirect user to logout endpoint
+    params = {'returnTo': url_for('home', _external=True), 'client_id': 'YOUR_CLIENT_ID'}
+    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
+
+
+def requires_auth(f):
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    if 'profile' not in session:
+      # Redirect to Login page here
+      return redirect('/')
+    return f(*args, **kwargs)
+
+  return decorated
+
+
+@app.route('/user_dashboard')
+@requires_auth
+def dashboard():
+    return render_template('dashboard.html',
+                           userinfo=session['profile'],
+                           userinfo_pretty=json.dumps(session['jwt_payload'], indent=4))
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/estimate', methods=['POST'])
 def estimate():
